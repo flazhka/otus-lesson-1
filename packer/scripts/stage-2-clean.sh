703b63f892de50723f539d97c9992b9851574ce2
Pawel Anchakov <flazhka@gmail.com>
2023-07-11 23:41:48 +0300

    Update stage-2-clean.sh

--- packer/scripts/stage-2-clean.sh
+++ packer/scripts/stage-2-clean.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Обновление и очистка всех ненужных пакетов
-yum update -y
-yum clean all
-
+yum update -y && yum clean all
 
 # Добавление ssh-ключа для пользователя vagrant
 mkdir -pm 700 /home/vagrant/.ssh
 curl -sL https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
-
 
 # Удаление временных файлов
 rm -rf /tmp/*
