d755c7477a3425155393448043430c5b17c30044
Pawel Anchakov <flazhka@gmail.com>
2023-09-09 22:19:59 +0300

    Update repo_server_script.sh

--- Lab7/repo_server_script.sh
+++ Lab7/repo_server_script.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
 sudo -i
 yum install -y \
     wget \
     nginx \
     createrepo \
     yum-utils 
-
+sed -i 's@index.htm;@index.htm;\n        autoindex on;@g' /root/rpmbuild/SOURCES/nginx.vh.default.conf
 systemctl enable nginx --now
 mkdir -p /usr/share/nginx/html/repos
-
-wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.14.1-1.el7_4.ngx.src.rpm -O /usr/share/nginx/html/repo/nginx-1.14.1-1.el7_4.ngx.src.rpm
-wget https://downloads.percona.com/downloads/percona-release/percona-release-1.0-6/redhat/percona-release-1.0-6.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-0.1-6.noarch.rpm
-
-createrepo -v /usr/share/nginx/html/repo/
+wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.14.1-1.el7_4.ngx.src.rpm -O /usr/share/nginx/html/repos/nginx-1.14.1-1.el7_4.ngx.src.rpm
+wget https://downloads.percona.com/downloads/percona-release/percona-release-1.0-6/redhat/percona-release-1.0-6.noarch.rpm -O /usr/share/nginx/html/repos/percona-release-0.1-6.noarch.rpm
+createrepo -v /usr/share/nginx/html/repos/
 
